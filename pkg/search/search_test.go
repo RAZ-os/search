04f813874ffb0e291481eaade2b392c144ad13d1
Firuzjon.Saidov <firuzdzhonsa@gmail.com>
2020-10-27 09:51:04 +0500

    изменил путь к файлу

--- pkg/search/search_test.go
+++ pkg/search/search_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestAll_success(t *testing.T) {
-	ch := All(context.Background(), "file1", []string{"../data/file1.txt"})
+	ch := All(context.Background(), "file1", []string{"../../data/file1.txt"})
 	_, ok := <-ch
 	if !ok {
 		t.Errorf("error: %v", ok)
